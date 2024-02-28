899ca4dfe2c4c2d4db3efe8e5ad007fe1f99e88b
thanhnguyen <thanhnguyen@mobcec.com>
2024-02-28 13:50:11 +0700

    added organization routes

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,59 @@
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+
+export const Navbar = () => {
+  return (
+    <nav className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center px-4">
+      {/* TODO: Mobile sidebar */}
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+        <Button
+          size="sm"
+          variant="primary"
+          className="hidden rounded-sm md:block h-auto py-1.5 px-2"
+        >
+          Create
+        </Button>
+        <Button
+          size="sm"
+          variant="primary"
+          className="rounded-sm md:hidden h-auto"
+        >
+          <Plus className="w-4 h-4" />
+        </Button>
+      </div>
+      <div className="ml-auto flex items-center gap-x-2">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl="organization/:id"
+          afterSelectOrganizationUrl="organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            },
+          }}
+        />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                width: 30,
+                height: 30,
+              },
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+};
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,29 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/webhook"],
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) {
+      // If the user is signed in, and the route is public
+      let path = "/select-org";
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+    if (!auth.userId && !auth.isPublicRoute) {
+      // If the user is not signed in, and the route is not public
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      // If the user is logged in, and they don't have an org, redirect them to select-org
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
   // Routes that can always be accessed, and have
   // no authentication information
   // ignoredRoutes: ["/"],

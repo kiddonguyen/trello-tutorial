42377c94fddd6081babc0be12799cd113d7e9a81
thanhnguyen <thanhnguyen@mobcec.com>
2024-03-02 11:52:34 +0700

    updated board list for dashboard

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,13 +1,16 @@
 import { Separator } from "@/components/ui/separator";
-import { Info } from "./info";
+import { Suspense } from "react";
 import { BoardList } from "./_components/board-list";
+import { Info } from "./info";
 const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info />
       <Separator className="my-4" />
       <div className="px-2 md:px-4">
-        <BoardList />
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
       </div>
     </div>
   );

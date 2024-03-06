b8247129729c8fd9c3b421b2d7de2e5c4da07068
thanhnguyen <thanhnguyen@mobcec.com>
2024-03-06 14:18:47 +0700

    Added: card modal

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,12 +1,17 @@
-import { Toaster } from "sonner";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Toaster } from "sonner";
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   // Create layout and provider for route that need clerk auth
   return (
     <ClerkProvider>
-      <Toaster />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };

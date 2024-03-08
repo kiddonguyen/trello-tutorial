108c46ab5da9dcd41c1cad8538ec6c2b25a3826c
thanhnguyen <thanhnguyen@mobcec.com>
2024-03-08 16:59:24 +0700

    Added: Stripe + Board Limits

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -1,18 +1,28 @@
 "use server";
+import { createAuditLog } from "@/lib/create-audit-log";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { db } from "@/lib/db";
+import { incrementAvailableCount, hasAvailableCount } from "@/lib/org-limits";
 import { auth } from "@clerk/nextjs";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { CreateBoard } from "./schema";
 import { InputType, ReturnType } from "./types";
-import { createAuditLog } from "@/lib/create-audit-log";
-import { ACTION, ENTITY_TYPE } from "@prisma/client";
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
+  
   if (!userId || !orgId) {
     return {
       error: "Unauthorized",
     };
+  }
+
+  const canCreate = await hasAvailableCount();
+
+  if (!canCreate) {
+    return {
+      error: "You have reached the maximum number of boards! Please upgrade to create more.",
+    }
   }
 
   const { title, image } = data;
@@ -44,6 +54,8 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         imageLinkHTML,
       },
     });
+
+    await incrementAvailableCount();
   } catch (error) {
     return {
       error: "Failed to create!",
--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -9,6 +9,9 @@ import { InputType, ReturnType } from "./types";
 import { redirect } from "next/navigation";
 import { createAuditLog } from "@/lib/create-audit-log";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { decreaseAvailableCount } from "@/lib/org-limits";
+
+
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
 
@@ -27,6 +30,8 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         orgId,
       },
     });
+
+    await decreaseAvailableCount();
   } catch (error) {
     return {
       error: "Failed to delete!",
